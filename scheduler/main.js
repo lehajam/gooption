5b7227177cb3b58e70fc357685ae3b86650e0962
lehajam <Lehajam@gmail.com>
2020-05-12 05:15:11 +0200

    refacto: code cleanup

--- scheduler/main.js
+++ scheduler/main.js
@@ -10,95 +10,71 @@ var pricer = newPricer();
 setInterval(function(){
     console.log('Pricing all trades...');
 
-    fetch("http://localhost:8080/graphql", {
-        "method": "POST",
-        "headers": {
-          "content-type": "application/json"
-        },
-        "body": JSON.stringify({ query: `{
-            queryTrade {
-              contract {
-                ticker
-                index {
-                  quotes (order: { desc: datePublished }, first: 1) {
-                    ... on StockQuote {
-                      datePublished
-                      close
-                    }
-                  }
-                }
-                ... on EuropeanContract {
-                  strike
-                  expiry
-                  putcall
+    graphQuery(
+      "http://localhost:8080/graphql",
+      `{
+        queryTrade {
+          contract {
+            ticker
+            index {
+              quotes (order: { desc: datePublished }, first: 1) {
+                ... on StockQuote {
+                  datePublished
+                  close
                 }
               }
             }
-          }` })
-    })
-    .then(res => res.json())
-    .then(jsonres => {
+            ... on EuropeanContract {
+              strike
+              expiry
+              putcall
+            }
+          }
+        }
+      }`,
+      function(queryResponse) {
 
-        var price = pricer.Price();
-
-        price.on('data', function(response) {
-
-          var timestamp = (new Date()).toISOString();
-          console.log(`contract:${response.client_id}\ttype:${response.value_type}\tvalue:${response.value}`);
-          fetch("http://localhost:8080/graphql", {
-            "method": "POST",
-            "headers": {
-              "content-type": "application/json"
-            },
-            "body": JSON.stringify({ query: `mutation {
-              addPriceResult(input: [{
-                datePublished:"${timestamp}",
-                contract: { ticker: "${response.client_id}" },
-                value: ${response.value},
-                resultType: "${response.value_type}",
-                source: "gobs"
-              }]) {
-                priceresult {
-                  id
-                  value
-                }
-            }}`})
-          })
-          .then(res => res.json())
-          .then(json => {
-            console.log('inserted row');
-          });
-        });
-
-        price.on('error', function(e) {
-            console.log(JSON.stringify(e));
-        });
-
-        price.on('end', function() {
-            console.log('Trades have been priced successfully');
-            price.end();
-        });
-
+        const timestamp = (new Date()).toISOString();
         const pricingdate = Math.round((new Date()).getTime() / 1000);  // always in UTC
-        jsonres.data.queryTrade.forEach(function(trade){
-            price.write({
-                client_id: trade.contract.ticker,
-                pricingdate: pricingdate,
-                strike: trade.contract.strike,
-                expiry: Math.round(Date.parse(trade.contract.expiry) / 1000),
-                put_call: trade.contract.putcall,
-                spot: trade.contract.index[0].quotes[0].close, // check if no quotes
-                vol: 0.1,
-                rate: 0.01
-            });
+        const priceRequests = queryResponse.data.queryTrade.map(function (trade) {
+          return {
+            client_id: trade.contract.ticker,
+            pricingdate: pricingdate,
+            strike: trade.contract.strike,
+            expiry: Math.round(Date.parse(trade.contract.expiry) / 1000),
+            put_call: trade.contract.putcall,
+            spot: trade.contract.index[0].quotes[0].close, // check if no quotes
+            vol: 0.1,
+            rate: 0.01
+          };
         });
 
-        price.end();
-    })
-    .catch(err => {
-        console.log(err);
-    });
-}, 10000);
+        grpcStreamQuery(
+          pricer.Price(),
+          priceRequests,
+          function(response) {
+
+            console.log(`contract:${response.client_id}\ttype:${response.value_type}\tvalue:${response.value}`);
+
+            graphQuery(
+              "http://localhost:8080/graphql",
+              `mutation {
+                addPriceResult(input: [{
+                  datePublished:"${timestamp}",
+                  contract: { ticker: "${response.client_id}" },
+                  value: ${response.value},
+                  resultType: "${response.value_type}",
+                  source: "gobs"
+                }]) {
+                  priceresult {
+                    id
+                    value
+                  }
+              }}`);
+          });
+      }
+    );
+ }, 10000);
 
 
 app.listen(4200);
@@ -116,4 +92,51 @@ function newPricer() {
     var gobs = grpc.loadPackageDefinition(packageDefinition).gobs;
     var client = new gobs.PricerService(':5050', grpc.credentials.createInsecure());
     return client;
+}
+
+function graphQuery(url, queryString, callback, errHandler) {
+  fetch(url, {
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json"
+    },
+    "body": JSON.stringify({query: queryString})
+  })
+  .then(res => res.json())
+  .then(jsonres => {
+    // console.json(JSON.stringify(jsonres));
+    if(callback && typeof callback === "function") {
+      callback(jsonres);
+    }
+  })
+  .catch(err => {
+    console.json(JSON.stringify(err));
+    if(errHandler && typeof errHandler === "function") {
+      errHandler(err);
+    }
+  });
+}
+
+function grpcStreamQuery(stub, requests, callback, errHandler, endHandler) {
+  stub.on('data', callback);
+
+  stub.on('error', function(err){
+    console.json(JSON.stringify(err));
+    if(errHandler && typeof errHandler === "function") {
+      errHandler(err);
+    }
+  });
+
+  stub.on('end', function(){
+    if(endHandler && typeof endHandler === "function") {
+      stub.end();
+      endHandler();
+    }
+  });
+
+  requests.forEach(function(req){
+      stub.write(req);
+  });
+
+  stub.end();
 }
